fdba465373abd5a94aaf62fbddf81b3d2b00748d
ernie <ernie@epicbytes.net>
2012-10-20 00:04:23 +0200

    complaints, complaints

--- js/scripts-ck.js
+++
@@ -1,1 +0,0 @@
-$(function(){$('.nav a[href^="#"]').click(function(e){e.preventDefault();$("html,body").animate({scrollTop:$(this.hash).offset().top-10},200);return!1})});$(document).ready(function(){$(".row").each(function(){$(this).css({height:$(window).height(),"padding-top":$(window).height()/2-$(".row div").height()/2-50})})});
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-$(function() {
-    $('.nav a[href^="#"]').click(function(e) {
-        e.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top}, 200);
-        return false;
-    });
-});
-
 $(document).ready(function(){
-    $('.row').each(function() {
-        $(this).css({
-            'height': $(window).height(),
-            'padding-top': $(window).height()/2-$('.row div').height()/2-50
-        });
-    });
+    // stuff
 });
