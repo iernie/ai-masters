1c7bf34863328698ea70dc0683fd64c4d2408341
ernie <ernie@epicbytes.net>
2012-10-19 21:48:45 +0200

    much reoganization, new layout, files included

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.nav a[href^="#"]').click(function(e) {
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top-10}, 200);
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.row').each(function() {
+        $(this).css({
+            'height': $(window).height(),
+            'padding-top': $(window).height()/2-$('.row div').height()/2-50
+        });
+    });
+});
